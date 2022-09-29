7cb4fc8e338fae35d18b5a35d498513322c71143
EkaterinaManuilova <e.manuilova@autosyst.ru>
2022-09-29 15:24:57 +0300

    левое меню

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,11 +2,14 @@ const form  = document.getElementsByTagName('form')[0];
 
 const email = document.getElementById('mail');
 const emailError = document.querySelector('#mail + span.subscribe-form__input-error');
+// const subscrButton = document.querySelector('subscribe-form__button');
 
 email.addEventListener('input', function (event) {
   if (email.validity.valid) {
+    email.className = 'subscribe-form__input valid';
     emailError.textContent = '';
     emailError.className = 'subscribe-form__input-error';
+    // subscrButton.className = 'subscribe-form__button';
   } else {
     showError();
   }
@@ -23,9 +26,11 @@ function showError() {
   if(email.validity.valueMissing) {
     emailError.textContent = 'Введите адрес электронной почты.';
   } else if(email.validity.typeMismatch) {
-    emailError.textContent = 'Введенные данные не являются электронной почтой.';
+    emailError.textContent = 'Не правильные данные';
   } else if(email.validity.tooShort) {
-    emailError.textContent = `Email должен содержать ${ email.minLength } символов; Вы ввели ${ email.value.length }.`;
+    emailError.textContent = `Email должен содержать ${ email.minLength } символов.`;
   }
+  email.className = 'subscribe-form__input invalid';
   emailError.className = 'subscribe-form__input-error active';
+  // subscrButton.className = 'subscribe-form__button disabled';
 }
