368f482aeb7f2d88d5542ff2dba1fa2c5801800b
EkaterinaManuilova <e.manuilova@autosyst.ru>
2022-10-20 15:11:39 +0300

    шаблоны для меню и статей

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,227 @@
+
+const leftMenuItems = [
+  {
+      link: '#',
+      name: 'Бейджи',
+      count: '14',
+      submenu: '',
+  },
+  {
+      link: '#',
+      name: 'Бумаги и картон',
+      count: '898',
+      submenu: 'true',
+  },
+  {
+      link: '#',
+      name: 'Конверты',
+      count: '10',
+      submenu: 'true',
+  },
+  {
+      link: '#',
+      name: 'Коробки и пакеты',
+      count: '42',
+      submenu: 'true',
+  },
+  {
+      link: '#',
+      name: 'Магнитный винил',
+      count: '14',
+      submenu: 'true',
+  },
+  {
+      link: '#',
+      name: 'Материалы для календарей',
+      count: '5',
+      submenu: 'true',
+  },
+  {
+      link: '#',
+      name: 'Материалы для наружной рекламы',
+      count: '95',
+      submenu: 'true',
+  },
+  {
+      link: '#',
+      name: 'Материалы для офсетной печати',
+      count: '10',
+      submenu: 'true',
+  },
+  {
+      link: '#',
+      name: 'Материалы для переплета',
+      count: '109',
+      submenu: 'true',
+  },
+  {
+      link: '#',
+      name: 'Материалы для пигментной печати',
+      count: '89',
+      submenu: '',
+  },
+  {
+      link: '#',
+      name: 'Материалы для ризографов',
+      count: '3',
+      submenu: '',
+  },
+  {
+      link: '#',
+      name: 'Материалы для сублимации',
+      count: '30',
+      submenu: '',
+  },
+  {
+      link: '#',
+      name: 'Материалы для шелкотрафаретной печати',
+      count: '61',
+      submenu: '',
+  },
+  {
+      link: '#',
+      name: 'Оборудование',
+      count: '151',
+      submenu: '',
+  },
+  {
+      link: '#',
+      name: 'Пленка для ламинирования',
+      count: '26',
+      submenu: '',
+  },
+  {
+      link: '#',
+      name: 'Прочие товары',
+      count: '49',
+      submenu: '',
+  },
+  {
+      link: '#',
+      name: 'Ручки для пакетов',
+      count: '4',
+      submenu: '',
+  },
+  {
+      link: '#',
+      name: 'Сувенирная продукция',
+      count: '4',
+      submenu: '',
+  },
+  {
+      link: '#',
+      name: 'Футболки, толстовки, бейсболки',
+      count: '26',
+      submenu: '',
+  },
+]
+
+const leftMenuContainer = document.querySelector('.left-menu__navbar-list');
+//загрузка левого меню
+function loadLeftMenu(linkValue, nameValue, countValue, subMenu) {
+  const leftMenuTemplate = document.querySelector('.left-menu-item-template').content;
+  console.log(leftMenuTemplate);
+  
+  const leftMenuElement = leftMenuTemplate.querySelector('.left-menu__navbar-item').cloneNode(true);
+  const leftMenuLink = leftMenuElement.querySelector('.left-menu__navbar-link-first-level');
+  const leftMenuName = leftMenuElement.querySelector('.menu-item-name');
+  const leftMenuCount = leftMenuElement.querySelector('.menu-item-count');
+  const leftMenuSub = leftMenuElement.querySelector('.menu-arrow');
+
+
+  leftMenuLink.setAttribute('href', linkValue);
+  leftMenuName.textContent = nameValue;
+  leftMenuCount.textContent = countValue;
+  
+  if (!subMenu) {
+    leftMenuSub.removeAttribute('class', leftMenuSub)
+  }
+
+
+  leftMenuContainer.append(leftMenuElement);
+}
+
+leftMenuItems.forEach((element) => {
+  loadLeftMenu(element.link, element.name, element.count, element.submenu);
+});
+
+const articleItems = [
+  {
+    name: 'Как выбрать биговщик',
+    text: `Биговка — это продавливание тонкой бороздки на листе бумаги, чтобы
+    его можно было аккуратно сложить. В типографии устройства для
+    биговки используются для нанесения бороздок на бумажные или
+    картонные листы плотностью Биговка — это продавливание тонкой
+    бороздки на листе бумаги, чтобы его можно было аккуратно сложить. В
+    типографии устройства для биговки используются для нанесения
+    бороздок на бумажные или картонные листы плотностью Биговка — это продавливание тонкой бороздки на листе бумаги, чтобы
+    его можно было аккуратно сложить. В типографии устройства для
+    биговки используются для нанесения бороздок на бумажные или
+    картонные листы плотностью Биговка — это продавливание тонкой
+    бороздки на листе бумаги, чтобы его можно было аккуратно сложить. В
+    типографии устройства для биговки используются для нанесения
+    бороздок на бумажные или картонные листы плотностью`,
+    link: '#'
+  },
+  {
+    name: 'какая бумага лучше?',
+    text: `Биговка — это продавливание тонкой бороздки на листе бумаги, чтобы
+    его можно было аккуратно сложить. В типографии устройства для
+    биговки используются для нанесения бороздок на бумажные или
+    картонные листы плотностью Биговка — это продавливание тонкой
+    бороздки на листе бумаги, чтобы его можно было аккуратно сложить. В
+    типографии устройства для биговки используются для нанесения
+    бороздок на бумажные или картонные листы плотностью`,
+    link: '#'
+  },
+  {
+    name: 'печатаем на одежде без проблем',
+    text: `Биговка — это продавливание тонкой бороздки на листе бумаги, чтобы
+    его можно было аккуратно сложить. В типографии устройства для
+    биговки используются для нанесения бороздок на бумажные или
+    картонные листы плотностью Биговка — это продавливание тонкой
+    бороздки на листе бумаги, чтобы его можно было аккуратно сложить. В
+    типографии устройства для биговки используются для нанесения
+    бороздок на бумажные или картонные листы плотностью`,
+    link: '#'
+  },
+  {
+    name: 'Как выбрать ризограф?',
+    text: `Биговка — это продавливание тонкой бороздки на листе бумаги, чтобы
+    его можно было аккуратно сложить. В типографии устройства для
+    биговки используются для нанесения бороздок на бумажные или
+    картонные листы плотностью Биговка — это продавливание тонкой
+    бороздки на листе бумаги, чтобы его можно было аккуратно сложить. В
+    типографии устройства для биговки используются для нанесения
+    бороздок на бумажные или картонные листы плотностью`,
+    link: '#'
+  }
+]
+
+const articlesContainer = document.querySelector('.articles__content');
+
+//загрузка статей
+function loadArticle(nameValue, textValue, linkValue) {
+  const articleTemplate = document.querySelector('.article-item-template').content;
+  
+  const articleElement = articleTemplate.querySelector('.article').cloneNode(true);
+  const articleLink = articleElement.querySelector('.article__link');
+  const articleName = articleElement.querySelector('.article__title');
+  const articleText = articleElement.querySelector('.article__text');
+
+  articleLink.setAttribute('href', linkValue);
+  articleName.textContent = nameValue;
+  articleText.textContent = textValue;
+
+  articlesContainer.append(articleElement);
+}
+
+articleItems.forEach((element) => {
+  loadArticle(element.name, element.text, element.link);
+});
+
+
+
 const form  = document.getElementsByTagName('form')[0];
 
 const email = document.getElementById('mail');
@@ -34,3 +258,4 @@ function showError() {
   emailError.className = 'subscribe-form__input-error active';
   // subscrButton.className = 'subscribe-form__button disabled';
 }
+
